3205249e67fededbc196d3466a370ab000adfbbc
Brandon Carlos <brandon_marzo@hotmail.com>
2025-08-23 23:25:40 -0300

    Use case for get created

--- devicesapi/src/main/java/com/devices/devicesapi/application/port/in/GetDeviceUseCase.java
+++ devicesapi/src/main/java/com/devices/devicesapi/application/port/in/GetDeviceUseCase.java
@@ -1,7 +1,6 @@
 package com.devices.devicesapi.application.port.in;
 
 import com.devices.devicesapi.application.dto.DeviceResponse;
-import com.devices.devicesapi.domain.model.DeviceState;
 
 import java.util.List;
 import java.util.UUID;
@@ -9,6 +8,5 @@ import java.util.UUID;
 public interface GetDeviceUseCase {
     DeviceResponse getById(UUID id);
     List<DeviceResponse> getAll();
-//    List<DeviceResponse> getByState(DeviceState state);
     List<DeviceResponse> getByBrand(String brand);
 }
